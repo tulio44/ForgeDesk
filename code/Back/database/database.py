68fea853cd1c219eaf4004c7c912ec13e73ed341
Túlio Henrique <tulio4421@gmail.com>
2026-05-11 21:04:44 -0300

    Adicionando configurações do database e main

--- code/Back/database/database.py
+++ code/Back/database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
--- code/Back/main.py
+++ code/Back/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 from api.solicitacoes import solicitacoes_bp
-from core.config import FLASK_PORT
+from core.config import FLASK_PORT, DEBUG
 from database.database import Base, engine
 from models.solicitacao import Solicitacao
 
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=FLASK_PORT,
-        debug=True
+        debug=DEBUG
     )
